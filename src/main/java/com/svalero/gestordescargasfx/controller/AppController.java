4b48f010d933b6be4a69f7b5bcb4e2b322fafcd0
Daniel G <dani75esp@gmail.com>
2023-01-03 19:02:56 +0100

    -show Log

--- src/main/java/com/svalero/gestordescargasfx/controller/AppController.java
+++ src/main/java/com/svalero/gestordescargasfx/controller/AppController.java
@@ -4,12 +4,13 @@ import com.svalero.gestordescargasfx.util.R;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.control.*;
 import javafx.scene.control.Button;
-import javafx.scene.control.Tab;
-import javafx.scene.control.TabPane;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.VBox;
 
+import java.awt.*;
+import java.io.File;
 import java.io.IOException;
 import java.util.HashMap;
 import java.util.Map;
@@ -39,6 +40,23 @@ public class AppController {
         String urlText = tfUrl.getText();
         tfUrl.clear();
         tfUrl.requestFocus();
+
+    }
+
+    @FXML
+    public void openLog(ActionEvent actionEvent) throws IOException, IllegalArgumentException {
+        try {
+            File file = new File("GestorDescargas.log");
+            Desktop.getDesktop().open(file);
+        } catch (IOException ioe) {
+            Alert alert = new Alert(Alert.AlertType.ERROR);
+            alert.setContentText("Ha habido un error.");
+            alert.show();
+        } catch (IllegalArgumentException iae) {
+            Alert alert = new Alert(Alert.AlertType.ERROR);
+            alert.setContentText("File not found.");
+            alert.show();
+        }
 
     }
 
